2041b861e8ca3b046624e5b0457367a9b8c30437
roasmarialaura <roa20003@byui.edu>
2025-11-08 17:05:20 -0300

    JS assignment1 done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,13 +5,9 @@ const lastModified = document.querySelector("#lastModified");
 
 const today = new Date();
 
-lastModified.innerHTML = `Last modified: ${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "lastModified"
-	}
-).format(today)};
 
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
 
 
 
