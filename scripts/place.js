65cd20f43e4fce80596e78498a59be7d5533481c
roasmarialaura <roa20003@byui.edu>
2026-02-13 10:23:14 -0300

    place js

---
+++ scripts/place.js
@@ -0,0 +1,10 @@
+
+const currentYear = document.querySelector('#currentYear');
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
