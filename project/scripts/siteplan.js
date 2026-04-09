3408c6f0786746a4d0580385cd30c7b9ed6a7c99
roasmarialaura <roa20003@byui.edu>
2026-04-09 09:49:58 -0300

    Siteplan js done

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,5 @@
+
+//Foother
+let today = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${today.getFullYear()}`;
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
